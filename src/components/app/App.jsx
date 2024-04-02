10601f7c7b612988127ad48d51b0377bf60e5cff
buzzler0109 <144600227+buzzler0109@users.noreply.github.com>
2024-04-02 05:43:03 +0300

    upd

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,24 +1,47 @@
-import Profile from "../profile/Profile";
-import userData from "../userData.json";
-import FriendsList from "../friends_list/FriendsList";
-import friends from "../friends.json";
-import TransactionHistory from "../transaction_history/TransactionHistory";
-import transactions from "../transactions.json";
+import { useEffect, useState } from "react";
+
+import SearchBox from "../search_box/SearchBox";
+import ContactForm from "../contact_form/ContactForm";
+import ContactList from "../contact_list/ContactList";
+import data from "../../data.json";
+
 import "./App.css";
 
 const App = () => {
+  const [contacts, setContact] = useState(() => {
+    const savedContacts = window.localStorage.getItem("contacts");
+    return savedContacts ? JSON.parse(savedContacts) : data;
+  });
+
+  const [finder, setFinder] = useState("");
+
+  useEffect(() => {
+    localStorage.setItem("contacts", JSON.stringify(contacts));
+  }, [contacts]);
+
+  const addContact = (newContact) => {
+    setContact((prevContacts) => {
+      return [...prevContacts, newContact];
+    });
+  };
+
+  const filteredContacts = contacts.filter((contact) =>
+    contact.name.toLowerCase().includes(finder.toLowerCase())
+  );
+
+  const deleteContact = (contactId) => {
+    setContact((prevContacts) => {
+      return prevContacts.filter((contact) => contact.id !== contactId);
+    });
+  };
+
   return (
-    <>
-      <Profile
-        name={userData.username}
-        tag={userData.tag}
-        location={userData.location}
-        image={userData.avatar}
-        stats={userData.stats}
-      />
-      <FriendsList friends={friends} />
-      <TransactionHistory items={transactions} />
-    </>
+    <div className="wrapper container form-body">
+      <h1 className="form-title">Phonebook</h1>
+      <ContactForm onAdd={addContact} />
+      <SearchBox value={finder} onFind={setFinder} />
+      <ContactList contacts={filteredContacts} onDelete={deleteContact} />
+    </div>
   );
 };
 
